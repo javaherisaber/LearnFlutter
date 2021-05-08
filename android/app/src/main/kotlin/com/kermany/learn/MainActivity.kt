d9a6f49f91e454d888d27f04a972cf759c1d5ea1
Mahdi Javaheri Saber <m.javaherisaber@gmail.com>
2021-05-08 14:38:32 +0430

    Create ImagePegs app

---
+++ android/app/src/main/kotlin/com/kermany/learn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kermany.learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:learn/src/imagepegs/app.dart';
+
+void main() {
+  runApp(App());
+}
