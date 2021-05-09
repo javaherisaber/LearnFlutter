00326a34a2e71563c9efc14e5915ee85a74fd41c
Mahdi <m.javaherisaber@gmail.com>
2021-05-09 05:30:42 +0430

    Add validator to stateful login screen

---
+++ lib/src/login_stateful/mixins/validation_mixin.dart
@@ -0,0 +1,17 @@
+mixin ValidationMixin {
+
+  String validateEmail(String value) {
+    // just a simple check to showcase validator
+    if (!value.contains('@')) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  String validatePassword(String value) {
+    if (value.length < 4) {
+      return 'Password must be at least 4 characters';
+    }
+    return null;
+  }
+}
--- lib/src/login_stateful/screens/login_screen.dart
+++ lib/src/login_stateful/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn/src/login_stateful/mixins/validation_mixin.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -7,8 +8,11 @@ class LoginScreen extends StatefulWidget {
   }
 }
 
-class LoginScreenState extends State<LoginScreen> {
+class LoginScreenState extends State<LoginScreen> with ValidationMixin {
   final formKey = GlobalKey<FormState>();
+
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
@@ -30,14 +34,15 @@ class LoginScreenState extends State<LoginScreen> {
 
   Widget emailField() {
     return TextFormField(
-      validator: (input) {
-        // todo: implement this validator
-      },
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
         labelText: 'Email Address',
         hintText: 'you@example.com',
       ),
+      validator: validateEmail,
+      onSaved: (String value) {
+        email = value;
+      },
     );
   }
 
@@ -46,6 +51,10 @@ class LoginScreenState extends State<LoginScreen> {
       obscureText: true,
       obscuringCharacter: '*',
       decoration: InputDecoration(labelText: 'Password', hintText: 'Password'),
+      validator: validatePassword,
+      onSaved: (String value) {
+        password = value;
+      },
     );
   }
 
@@ -56,7 +65,11 @@ class LoginScreenState extends State<LoginScreen> {
       ),
       child: Text('Submit'),
       onPressed: () {
-        formKey.currentState.validate();
+        var isValid = formKey.currentState.validate();
+        if (isValid) {
+          formKey.currentState.save();
+          print('Time to post $email and $password to api');
+        }
       },
     );
   }
