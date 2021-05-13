f479c3e2523e09c6fdaf2c937f574bfa8016f0ca
Mahdi <m.javaherisaber@gmail.com>
2021-05-13 09:04:25 +0430

    Implement login with Global instance bloc

--- lib/src/login_bloc/blocs/bloc.dart
+++ lib/src/login_bloc/blocs/bloc.dart
@@ -16,3 +16,5 @@ class Bloc with Validators {
     _password.close();
   }
 }
+
+final bloc = Bloc();
--- lib/src/login_bloc/screens/login_screen.dart
+++ lib/src/login_bloc/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn/src/login_bloc/blocs/bloc.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -17,22 +18,36 @@ class LoginScreen extends StatelessWidget {
   }
 
   Widget emailField() {
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        hintText: 'you@example.com',
-        labelText: 'Email Address',
-      ),
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'you@example.com',
+            labelText: 'Email Address',
+            errorText: snapshot.error,
+          ),
+        );
+      },
     );
   }
 
   Widget passwordField() {
-    return TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: 'Password',
-        labelText: 'Password',
-      ),
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            labelText: 'Password',
+            errorText: snapshot.error,
+          ),
+        );
+      },
     );
   }
 
