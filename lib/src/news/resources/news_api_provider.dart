4fbeecf50596faa38e41328bd2f020ad7a0758dd
Mahdi Javaheri Saber <m.javaherisaber@gmail.com>
2021-05-26 17:57:33 +0430

    Implement news app database

--- lib/src/news/resources/news_api_provider.dart
+++ lib/src/news/resources/news_api_provider.dart
@@ -1,19 +1,19 @@
 import 'package:http/http.dart' show Client;
 import 'dart:convert';
-
 import 'package:learn/src/news/models/item_model.dart';
+import 'dart:async';
 
 class NewsApiProvider {
   static final _baseUrl = 'https://hacker-news.firebaseio.com/v0';
   Client client = Client();
 
-  fetchTopIds() async {
+  Future<List<int>> fetchTopIds() async {
     final response = await client.get(Uri.parse('$_baseUrl/topstories.json'));
     final ids = json.decode(response.body);
-    return ids;
+    return ids.cast<int>();
   }
 
-  fetchItem(int id) async {
+  Future<ItemModel> fetchItem(int id) async {
     final response = await client.get(Uri.parse('$_baseUrl/item/$id.json'));
     final parsedJson = json.decode(response.body);
     return ItemModel.fromJson(parsedJson);
